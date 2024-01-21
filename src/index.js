154f65a3445e99b014073d2ad9520cc32ef50d21
luis92guimaraes <luis92guimaraes@outlook.com>
2024-01-21 12:39:29 -0300

    "primeiro teste "

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const nome = "Kratos";
+const xp = 8900;
+
+if(xp < 1000){
+    console.log(nome + " está no nível Ferro!")
+} else if (xp < 2000) {
+    console.log(nome + " está no nível Bronze!")
+} else if (xp < 5000) {
+    console.log(nome + " está no nível Prata!")
+} else if (xp < 7000) {
+    console.log(nome + " está no nível Ouro!")
+} else if (xp < 9000) {
+    console.log(nome + " está no nível Platina!")
+} else if (xp < 11000) {
+    console.log(nome + " está no nível Ascendente!")
+} else if (xp < 13000) {
+    console.log(nome + " está no nível Radiante!")
+} else (xp < 14000)
+    console.log(nome + " está no nível Imortal!");
