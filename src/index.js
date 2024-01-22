0976eca97dcee6ca3622d001d3b574941375ddde
luis92guimaraes <luis92guimaraes@outlook.com>
2024-01-22 20:03:10 -0300

    desafio concluido

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 const nome = "Kratos";
-const xp = 8900;
+const xp = 15000;
 
-if(xp < 1000){
+if(xp <= 1000){
     console.log(nome + " está no nível Ferro!")
-} else if (xp < 2000) {
+} else if (xp >= 1001 && xp <= 2000) {
     console.log(nome + " está no nível Bronze!")
-} else if (xp < 5000) {
+} else if (xp >= 2001 && xp <= 5000) {
     console.log(nome + " está no nível Prata!")
-} else if (xp < 7000) {
+} else if (xp >= 5001 && xp <= 7000) {
     console.log(nome + " está no nível Ouro!")
-} else if (xp < 9000) {
+} else if (xp >= 7001 && xp <= 9000) {
     console.log(nome + " está no nível Platina!")
-} else if (xp < 11000) {
+} else if (xp >= 9001 && xp <= 11000) {
     console.log(nome + " está no nível Ascendente!")
-} else if (xp < 13000) {
+} else if (xp >= 11001 && xp <= 13000) {
     console.log(nome + " está no nível Radiante!")
-} else (xp < 14000)
-    console.log(nome + " está no nível Imortal!");
+} else {
+    console.log(nome + " está no nível Imortal!")
+}
